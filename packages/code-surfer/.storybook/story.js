8dfc54d752f3a7bea325d6a78ea3eb2c53710c11
pomber <pombopombopombo@gmail.com>
2018-08-18 23:33:39 -0300

    Add prismjs

--- packages/code-surfer/.storybook/story.js
+++ packages/code-surfer/.storybook/story.js
@@ -2,6 +2,12 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import CodeSurfer from "../src/code-surfer";
 
+const code = `
+<div>
+  <span>Hi</span>
+</div>
+`;
+
 storiesOf("CodeSurfer", module).add("test hi", () => (
-  <CodeSurfer>Foo</CodeSurfer>
+  <CodeSurfer code={code} />
 ));
--- packages/code-surfer/src/code-surfer.js
+++ packages/code-surfer/src/code-surfer.js
@@ -1,3 +1,36 @@
 import React from "react";
+import Prism from "prismjs";
 
-export default ({ children }) => <div>Hi {children}</div>;
+import "prismjs/components/prism-jsx.js";
+import "prismjs/themes/prism.css";
+
+const parseCode = code => {
+  const grammar = Prism.languages["jsx"];
+  const language = null;
+  const env = {
+    code: code,
+    grammar: grammar,
+    language: language
+  };
+  Prism.hooks.run("before-tokenize", env);
+  env.tokens = Prism.tokenize(env.code, env.grammar);
+  Prism.hooks.run("after-tokenize", env);
+
+  return Prism.Token.stringify(Prism.util.encode(env.tokens), env.language);
+};
+
+class CodeSurfer extends React.Component {
+  render() {
+    return (
+      <pre>
+        <code
+          dangerouslySetInnerHTML={{
+            __html: parseCode(this.props.code)
+          }}
+        />
+      </pre>
+    );
+  }
+}
+
+export default CodeSurfer;
