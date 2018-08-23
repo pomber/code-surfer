aa3a10357d4298ef625710ab008e4f4b6895f834
Rodrigo Pombo <pombopombopombo@gmail.com>
2018-08-23 16:14:41 -0300

    Edit /packages/code-surfer/src/step-parser.test.js

--- packages/code-surfer/src/step-parser.test.js
+++ packages/code-surfer/src/step-parser.test.js
@@ -9,8 +9,8 @@ test("map lines", () => {
 });
 
 test("map range", () => {
-  const steps = [{ range: [1, 1] }, { range: [1, 1], note: "foo" }];
-  const expected = [{ 1: null, 2: null, 3: null }, { 1: null }];
+  const steps = [{ range: [1, 3] }, { range: [1, 1], note: "foo" }];
+  const expected = [{ 1: [], 2: null, 3: null }, { 1: null }];
 
   const tokenSteps = steps.map(mapStep);
   expect(tokenSteps).toEqual(expected);
