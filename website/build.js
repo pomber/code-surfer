0da53a948c9c5b44b5911f0bf272db15e0aa66b8
Rodrigo Pombo <pombopombopombo@gmail.com>
2018-09-07 17:50:36 -0300

    Fix folder copy

--- website/build.js
+++ website/build.js
@@ -1,15 +1,17 @@
 const fs = require("fs");
 const path = require("path");
+const ncp = require("ncp").ncp;
+ncp.limit = 1;
 
 const fixturesPath = path.resolve(__dirname, "../fixtures");
 const publicPath = path.resolve(__dirname, "./public");
 const distPath = path.resolve(__dirname, "./dist");
 
-copyFolderRecursiveSync(publicPath, distPath);
+copyFolder(publicPath, distPath);
 
 const fixtures = fs.readdirSync(fixturesPath);
 fixtures.forEach(fixture => {
-  copyFolderRecursiveSync(
+  copyFolder(
     path.join(fixturesPath, fixture, "dist"),
     path.join(distPath, fixture)
   );
@@ -17,38 +19,14 @@ fixtures.forEach(fixture => {
 
 // utils
 
-function copyFileSync(source, target) {
-  var targetFile = target;
-
-  //if target is a directory a new file with the same name will be created
-  if (fs.existsSync(target)) {
-    if (fs.lstatSync(target).isDirectory()) {
-      targetFile = path.join(target, path.basename(source));
+function copyFolder(source, target) {
+  if (!fs.existsSync(target)) {
+    fs.mkdirSync(target);
+  }
+  ncp(source, target, function(err) {
+    if (err) {
+      return console.error(err);
     }
-  }
-
-  fs.writeFileSync(targetFile, fs.readFileSync(source));
-}
-
-function copyFolderRecursiveSync(source, target) {
-  var files = [];
-
-  //check if folder needs to be created or integrated
-  var targetFolder = target;
-  if (!fs.existsSync(targetFolder)) {
-    fs.mkdirSync(targetFolder);
-  }
-
-  //copy
-  if (fs.lstatSync(source).isDirectory()) {
-    files = fs.readdirSync(source);
-    files.forEach(function(file) {
-      var curSource = path.join(source, file);
-      if (fs.lstatSync(curSource).isDirectory()) {
-        copyFolderRecursiveSync(curSource, targetFolder);
-      } else {
-        copyFileSync(curSource, targetFolder);
-      }
-    });
-  }
+    console.log(`${target} done.`);
+  });
 }
