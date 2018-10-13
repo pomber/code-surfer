9bfe2ccad98b465bdee6eca7a3282ecf25c62d0a
pomber <pombopombopombo@gmail.com>
2018-10-13 18:28:45 -0300

    Create ssr fixture

---
+++ fixtures/react-ssr/src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import CodeSurfer from "code-surfer";
+const App = () => (
+  <div style={{ height: 100 }}>
+    <CodeSurfer code={`console.log();`} />
+  </div>
+);
+
+export default App;
