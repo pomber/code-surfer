1c6e81f12cc2a4ec8cfb88d9846ab01b17debc05
pomber <pombopombopombo@gmail.com>
2018-12-20 14:46:55 -0300

    Remove clock from presenter mode test

--- fixtures/sample/cypress/integration/sample.spec.js
+++ fixtures/sample/cypress/integration/sample.spec.js
@@ -1,4 +1,5 @@
 function testSlides() {
+  // Keep moving right and snapshoting until the hash doesn't change
   cy.hash().then(currentHash => {
     cy.matchImageSnapshot(currentHash.replace(".", "-"));
     cy.get("body")
@@ -23,6 +24,15 @@ describe("Sample Deck", function() {
 
   it("works in presenter mode", function() {
     cy.visit(`${url}/?mode=presenter#9`).wait(300);
+    // cy.clock(new Date(2018).getTime());
+
+    // Hide clock
+    cy.get("button")
+      .contains("start")
+      .then(([button]) => {
+        button.parentElement.style.display = "none";
+      });
+
     cy.matchImageSnapshot("presenter-9");
   });
 
