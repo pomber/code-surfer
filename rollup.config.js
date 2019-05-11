cde2581059cc5f381371c5646980408dabc24ee8
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-05-11 21:10:22 +0200

    Run prettier

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,24 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import resolve from "rollup-plugin-node-resolve";
+import url from "rollup-plugin-url";
+import svgr from "@svgr/rollup";
 
-import pkg from './package.json'
+import pkg from "./package.json";
 
 export default {
-  input: 'src/index.js',
+  input: "src/index.js",
   output: [
     {
       file: pkg.main,
-      format: 'cjs',
+      format: "cjs",
       sourcemap: true
     },
     {
       file: pkg.module,
-      format: 'es',
+      format: "es",
       sourcemap: true
     }
   ],
@@ -30,10 +30,10 @@ export default {
     url(),
     svgr(),
     babel({
-      exclude: 'node_modules/**',
-      plugins: [ 'external-helpers' ]
+      exclude: "node_modules/**",
+      plugins: ["external-helpers"]
     }),
     resolve(),
     commonjs()
   ]
-}
+};
--- src/playhead/playhead.js
+++ src/playhead/playhead.js
@@ -1,104 +1,104 @@
-import easing from "./easing"
-const MULTIPLY = "multiply"
+import easing from "./easing";
+const MULTIPLY = "multiply";
 
 /* eslint-disable */
 function mergeResults(results, composite) {
-  const firstResult = results[0]
+  const firstResult = results[0];
   if (results.length < 2) {
-    return firstResult
+    return firstResult;
   }
   if (Array.isArray(firstResult)) {
     return firstResult.map((_, i) => {
-      return mergeResults(results.map(result => result[i]), composite)
-    })
+      return mergeResults(results.map(result => result[i]), composite);
+    });
   } else {
-    const merged = Object.assign({}, ...results)
+    const merged = Object.assign({}, ...results);
 
     if (composite === MULTIPLY) {
-      const opacities = results.map(x => x.opacity).filter(x => x != null)
+      const opacities = results.map(x => x.opacity).filter(x => x != null);
       if (opacities.length !== 0) {
-        merged.opacity = opacities.reduce((a, b) => a * b)
+        merged.opacity = opacities.reduce((a, b) => a * b);
       }
     }
-    return merged
+    return merged;
   }
 }
 
 const playhead = {
   tween: (props, context) => t => {
-    const { from, to, ease = easing.linear } = props
+    const { from, to, ease = easing.linear } = props;
 
-    const style = {}
+    const style = {};
     Object.keys(from).forEach(key => {
-      const value = from[key] + (to[key] - from[key]) * ease(t)
+      const value = from[key] + (to[key] - from[key]) * ease(t);
       if (key === "x") {
-        style["transform"] = `translateX(${value}px)`
+        style["transform"] = `translateX(${value}px)`;
       } else {
-        style[key] = value
+        style[key] = value;
       }
-    })
+    });
 
-    return style
+    return style;
   },
   chain: ({ children: fns, durations }, ctx) => {
     return (t, ...args) => {
-      let style = run(fns[0], 0, ctx)
-      let lowerDuration = 0
+      let style = run(fns[0], 0, ctx);
+      let lowerDuration = 0;
       for (let i = 0; i < fns.length; i++) {
-        const fn = fns[i]
-        const thisDuration = durations[i]
-        const upperDuration = lowerDuration + thisDuration
+        const fn = fns[i];
+        const thisDuration = durations[i];
+        const upperDuration = lowerDuration + thisDuration;
         if (lowerDuration <= t && t <= upperDuration) {
-          const innerT = (t - lowerDuration) / thisDuration
-          style = mergeResults([style, run(fn, innerT, ctx)])
+          const innerT = (t - lowerDuration) / thisDuration;
+          style = mergeResults([style, run(fn, innerT, ctx)]);
         } else if (upperDuration < t) {
           // merge the end of previous animation
-          style = mergeResults([style, run(fn, 1, ctx)])
+          style = mergeResults([style, run(fn, 1, ctx)]);
         } else if (t < lowerDuration) {
           // merge the start of future animation
-          style = mergeResults([run(fn, 0, ctx), style])
+          style = mergeResults([run(fn, 0, ctx), style]);
         }
-        lowerDuration = upperDuration
+        lowerDuration = upperDuration;
       }
-      return style
-    }
+      return style;
+    };
   },
   delay: () => () => ({}),
   parallel: ({ children: fns }, ctx) => {
     return t => {
-      const styles = fns.map(fn => run(fn, t, ctx))
-      const result = mergeResults(styles, MULTIPLY)
-      return result
-    }
+      const styles = fns.map(fn => run(fn, t, ctx));
+      const result = mergeResults(styles, MULTIPLY);
+      return result;
+    };
   },
   list: ({ forEach: items, children }, ctx) => t => {
-    const mapper = children[0]
-    const results = items.map(mapper)
-    return results.map(element => run(element, t, ctx))
+    const mapper = children[0];
+    const results = items.map(mapper);
+    return results.map(element => run(element, t, ctx));
   }
-}
+};
 
 export function createAnimation(type, config, ...children) {
-  const props = { ...config, children }
+  const props = { ...config, children };
   return {
     type: typeof type === "string" ? playhead[type] : type,
     props
-  }
+  };
 }
 
 /* @jsx createAnimation */
 export const Stagger = (props, ctx) => t => {
-  const targets = props.targets
-  const filter = target => !props.filter || props.filter(target)
+  const targets = props.targets;
+  const filter = target => !props.filter || props.filter(target);
   const interval =
     targets.filter(filter).length < 2
       ? 0
-      : props.interval / (targets.filter(filter).length - 1)
-  let i = 0
+      : props.interval / (targets.filter(filter).length - 1);
+  let i = 0;
   return targets.map(target => {
     // console.log(target, props.filter(target));
     if (!filter(target)) {
-      return {}
+      return {};
     }
     const animation = (
       <parallel>
@@ -107,29 +107,29 @@ export const Stagger = (props, ctx) => t => {
           {props.children[0]}
         </chain>
       </parallel>
-    )
-    i++
-    const result = run(animation, t, ctx)
+    );
+    i++;
+    const result = run(animation, t, ctx);
     // console.log("Stagger Result", t, result);
-    return result
-  })
-}
+    return result;
+  });
+};
 
 export function Context() {
-  throw Error("shouldnt run Context")
+  throw Error("shouldnt run Context");
 }
 
 export function run(node, t, context = {}) {
   if (node.type === Context) {
-    const { children, ...patch } = node.props
-    const newContext = { ...context, ...patch }
-    return run(children[0], t, newContext)
+    const { children, ...patch } = node.props;
+    const newContext = { ...context, ...patch };
+    return run(children[0], t, newContext);
   }
 
-  const result = node.type(node.props, context)
+  const result = node.type(node.props, context);
   if (result.type) {
-    return run(result, t, context)
+    return run(result, t, context);
   } else {
-    return result(t)
+    return result(t);
   }
 }
