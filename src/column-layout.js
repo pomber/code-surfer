43c6adf48ea4b9e36f4be9f9a032018b47060d8d
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-06-13 22:25:10 +0100

    Add step title and subtitle

--- src/column-layout.js
+++ src/column-layout.js
@@ -1,38 +1,57 @@
 import React from "react";
 import { readStepFromElement } from "./step-reader";
 import CodeSurfer from "./code-surfer";
+import { useSteps, useDeck } from "mdx-deck";
+import { useSubtitleStyle, useTitleStyle } from "./theming";
 
 function ColumnLayout({ children, themes, sizes }) {
-  const columns = getColumnsFromChildren(children, sizes, themes);
-  console.log(columns);
+  const deck = useDeck();
+  const [columns, titles, subtitles] = React.useMemo(
+    () => getColumnsFromChildren(children, sizes, themes),
+    [deck.index]
+  );
+  const stepIndex = useSteps(columns[0].length);
   return (
-    <div
-      style={{
-        width: "100vw",
-        maxWidth: "100%",
-        height: "100vh",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: "0.8em"
-      }}
-    >
-      {columns.map((column, i) => (
-        <div
-          key={i}
-          style={{ flex: column.flex, overflow: "hidden", height: "100%" }}
-        >
-          <CodeSurfer steps={column.steps} lang={column.steps[0].lang} />
-        </div>
-      ))}
-    </div>
+    <React.Fragment>
+      <div
+        style={{
+          width: "100vw",
+          maxWidth: "100%",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "0.8em"
+        }}
+      >
+        {columns.map((column, i) => (
+          <div
+            key={i}
+            style={{
+              flex: column.flex,
+              overflow: "hidden",
+              height: "100%"
+            }}
+          >
+            <CodeSurfer steps={column.steps} lang={column.steps[0].lang} />
+          </div>
+        ))}
+      </div>
+      {titles[stepIndex] && (
+        <div style={useTitleStyle()}>{titles[stepIndex]}</div>
+      )}
+      {subtitles[stepIndex] && (
+        <div style={useSubtitleStyle()}>{subtitles[stepIndex]}</div>
+      )}
+    </React.Fragment>
   );
 }
 
 function getColumnsFromChildren(children, sizes = []) {
   const columns = [];
-  React.Children.toArray(children).forEach((stepElement, stepIndex) => {
+  const stepElements = React.Children.toArray(children);
+  stepElements.forEach((stepElement, stepIndex) => {
     React.Children.toArray(stepElement.props.children).forEach(
       (codeElement, columnIndex) => {
         columns[columnIndex] = columns[columnIndex] || { steps: [] };
@@ -46,7 +65,11 @@ function getColumnsFromChildren(children, sizes = []) {
   columns.forEach((column, columnIndex) => {
     column.flex = sizes[columnIndex] || 1;
   });
-  return columns;
+
+  const titles = stepElements.map(stepElement => stepElement.props.title);
+  const subtitles = stepElements.map(stepElement => stepElement.props.subtitle);
+
+  return [columns, titles, subtitles];
 }
 
 export default ColumnLayout;
