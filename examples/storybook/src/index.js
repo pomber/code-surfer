6ec10dfea6bbeeb2c2aa97f1a40f846d073c0f7a
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-07-01 13:16:27 +0200

    Add storybook

---
+++ examples/storybook/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Button } from "@storybook/react/demo";
+
+storiesOf("Button", module)
+  .add("with text", () => <Button>Hello Button</Button>)
+  .add("with emoji", () => (
+    <Button>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
